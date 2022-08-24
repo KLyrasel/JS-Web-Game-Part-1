8d509a131a169f9fd4a9fd60e460e8746ab92adb
Klyrasel <KaiLyrasel@outlook.com>
2022-08-24 04:42:54 -0400

    second commit

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ newImage('assets/well.png', 350, 100)
 newImage('assets/rock.png', 150, 200)
 newImage('assets/pillar.png', 500, 425)
 
+function newItem(url,bottom,left){
+    let object = newImage(url,bottom,left)
+    object.addEventListener('click', function() {
+        object.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
