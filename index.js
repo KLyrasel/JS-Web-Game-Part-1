8e159cdadc604cd7d9fae5ed2e62764d05aeba42
Klyrasel <KaiLyrasel@outlook.com>
2022-08-24 04:34:37 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url,bottom,left,){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 200, 300)
+newImage('assets/well.png', 350, 100)
+newImage('assets/rock.png', 150, 200)
+newImage('assets/pillar.png', 500, 425)
+
